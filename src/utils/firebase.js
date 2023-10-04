58bf995b8acf0072fb7520cb11336df2721e0af2
Shubhashis Roy <shubhashisroy360@gmail.com>
2023-10-04 10:14:00 +0530

    icon setup

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,16 +8,17 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAPcEizuKW5ZPhezACRkO9B9WPvd1iNl4U",
-  authDomain: "netflixgpt-18b13.firebaseapp.com",
-  projectId: "netflixgpt-18b13",
-  storageBucket: "netflixgpt-18b13.appspot.com",
-  messagingSenderId: "367683936255",
-  appId: "1:367683936255:web:238a90f0e0e5bb084091ca",
-  measurementId: "G-MP3V52JQRR",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// eslint-disable-next-line
 const analytics = getAnalytics(app);
 export const auth = getAuth();
